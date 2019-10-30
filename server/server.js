b0b926490d9404a001955f669d8f5beac4f56c64
Guilherme Paz Silva <pazsilvag@gmail.com>
2019-10-30 18:10:35 -0300

    .

--- server/server.js
+++ server/server.js
@@ -78,6 +78,7 @@ app.get('/games', function (req, res) {
 	ok(res, response);
 });
 
-app.listen(3000, function () {
-	console.log('listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+	console.log(`Server listening on port ${port}`);
 });
